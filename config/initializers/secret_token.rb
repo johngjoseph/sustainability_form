ed0d93a831c5dcf724f8e4ca5124bc20b9b2e415
john.g.joseph <john.g.joseph@gmail.com>
2012-05-20 17:29:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SustainabilityForm::Application.config.secret_token = '69a16aaf82a34683d8cf5995ff8f2d5353c11a9ac6c87cac9338de6e8a315a6042815f0a73c29dd375c5f24485c5bf07d93c8cefe008a2186ecee911d7186231'
