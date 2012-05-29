783df4acb8d479145ced2243f67aedb893970e37
john.g.joseph <john.g.joseph@gmail.com>
2012-05-28 23:17:13 -0700

    Finish User Signup and Ch 7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120529014904) do
+ActiveRecord::Schema.define(:version => 20120529015910) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(:version => 20120529014904) do
     t.string   "name"
     t.string   "email"
     t.string   "organization"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
