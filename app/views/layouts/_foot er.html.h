428d4f2d0084e8e07bed5389255cbe3973ffefa8
john.g.joseph <john.g.joseph@gmail.com>
2012-05-28 17:50:08 -0700

    Finish layout and routes Ch. 5

---
+++ app/views/layouts/_foot er.html.h
@@ -0,0 +1,14 @@
+//
+//  _foot er.html.h
+//
+//  Created by John Joseph on 2012-05-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface _foot er.html : NSObject
+{
+}
++ (_foot er.html*)sharedInstance;
+@end
